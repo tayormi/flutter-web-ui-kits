bff273fcefe7f94f8b4af9ed7e5f9fd554998825
Israel <easyakowe@gmail.com>
2020-06-22 22:02:23 +0100

    updated scaffold_widget file

--- sign_in_page/lib/views/scaffold_widget.dart
+++ sign_in_page/lib/views/scaffold_widget.dart
@@ -4,37 +4,31 @@ import 'package:sign_in_page/views/centered_view.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 class ScaffoldWidget extends StatefulWidget {
-  
-  final double widthOfScreen;
-  final Color mainBackgroundColor;
-  final Color boxDecorationColor;
-  final Color signInTextColor;
-  final Color emailTextColor;
-  final Color textFieldBorderColor;
-  final Color hintTextColor;
-  final Color richTextColor;
-  final Color newOnOurPlatformColor;
-  final Color roundedRectangularBorderColor;
-  final Color materialButtonColor;
-  final Color materialButtonTextColor;
-  final Color bottomTextColor;
-
-  ScaffoldWidget({this.widthOfScreen, this.mainBackgroundColor, this.boxDecorationColor,
-  this.signInTextColor, this.emailTextColor, this.textFieldBorderColor, this.hintTextColor,
-  this.richTextColor, this.newOnOurPlatformColor, this.roundedRectangularBorderColor,
-  this.materialButtonColor, this.materialButtonTextColor, this.bottomTextColor});
-
-  
   @override
   _ScaffoldWidgetState createState() => _ScaffoldWidgetState();
 }
 
 class _ScaffoldWidgetState extends State<ScaffoldWidget> {
-  // ···
 
-  
+  Color mainBackgroundColor = Color(0xFFf5f6fa);
+  Color boxDecorationColor = Colors.white;
+  Color textFieldFillColor = Colors.white;
+  Color signInTextColor = Color(0xFF232e4b);
+  Color emailTextColor = Color(0xFF232e4b);
+  Color textFieldBorderColor = Color(0xFFC0C4CC);
+  Color hintTextColor = Color(0xFFC0C4CC);
+  Color richTextColor = Color(0xFF232e4b);
+  Color newOnOurPlatformColor = Color(0xFF232e4b);
+  Color roundedRectangularBorderColor = Color(0xFF232e4b);
+  Color materialButtonColor = Colors.white;
+  Color materialButtonTextColor = Color(0xFF232e4b);
+  Color bottomTextColor = Color(0xFFF43C7C);
+
   @override
   Widget build(BuildContext context) {
+
+    var widthOfScreen = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: mainBackgroundColor,
       body: CenteredView(
@@ -50,45 +44,48 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 8.0, right: 60.0),
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 8.0, left: 8.0, right: 60.0),
                       child: ToggleSwitch(
                         minWidth: 60.0,
-                        initialLabelIndex: 1,
+                        initialLabelIndex: 0,
+                        cornerRadius: 10.0,
                         activeBgColor: Color(0xFFF43C7C),
                         activeTextColor: Colors.white,
                         inactiveBgColor: Colors.grey,
                         inactiveTextColor: Colors.grey[900],
                         labels: ['Light', 'Dark'],
                         onToggle: (index) {
-                          setState ({
+                          setState(() {
                             if (index == 0) {
-                         
-  mainBackgroundColor = Colors.white;
-  boxDecorationColor = Colors.white;
-  signInTextColor = Colors.white;
-  emailTextColor = Colors.white;
-  textFieldBorderColor = Colors.white;
-  hintTextColor = Colors.white;
-  richTextColor = Colors.white;
-  newOnOurPlatformColor = Colors.white;
-  roundedRectangularBorderColor = Colors.white;
-  materialButtonColor = Colors.white;
-  materialButtonTextColor = Colors.white;
-  bottomTextColor = Colors.white;
-                              
+
+                              mainBackgroundColor = Color(0xFFf5f6fa);
+                              boxDecorationColor = Colors.white;
+                              textFieldFillColor = Colors.white;
+                              signInTextColor = Color(0xFF232e4b);
+                              emailTextColor = Color(0xFF232e4b);
+                              textFieldBorderColor = Color(0xFFC0C4CC);
+                              hintTextColor = Color(0xFFC0C4CC);
+                              richTextColor = Color(0xFF232e4b);
+                              newOnOurPlatformColor = Color(0xFF232e4b);
+                              roundedRectangularBorderColor = Color(0xFF232e4b);
+                              materialButtonColor = Colors.white;
+                              materialButtonTextColor = Color(0xFF232e4b);
+                              bottomTextColor = Color(0xFFF43C7C);
+
                             }else if (index == 1) {
-                              mainBackgroundColor = Colors.black;
-  boxDecorationColor = Colors.white;
-  signInTextColor = Colors.white;
-  emailTextColor = Colors.white70;
-  textFieldBorderColor = Color(0xFF8C8C94);
-  hintTextColor = Colors.white30;
-  richTextColor = Color(0xFF8C9494);
-  newOnOurPlatformColor = Color(0xFF8C9494);
-  roundedRectangularBorderColor = Color(0xFFF43C7C);
-  materialButtonColor = Colors.black54;
-  materialButtonTextColor = Color(0xFF8C9494);
-  bottomTextColor = Color(0xFF8C8C94);
+                              mainBackgroundColor = Color(0xFF1E1E21);
+                              boxDecorationColor = Colors.black54;
+                              textFieldFillColor = Colors.black54;
+                              signInTextColor = Colors.white;
+                              emailTextColor = Colors.white70;
+                              textFieldBorderColor = Color(0xFF8C8C94);
+                              hintTextColor = Colors.white30;
+                              richTextColor = Color(0xFF8C9494);
+                              newOnOurPlatformColor = Color(0xFF8C9494);
+                              roundedRectangularBorderColor = Color(0xFFF43C7C);
+                              materialButtonColor = Colors.black54;
+                              materialButtonTextColor = Color(0xFF8C9494);
+                              bottomTextColor = Color(0xFF8C8C94);
                             }
                           });
 
@@ -99,7 +96,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                   ],
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 30.0, right: 60.0, left: 60.0),
+                  padding: const EdgeInsets.only(top: 10.0, right: 60.0, left: 60.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -108,7 +105,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                               fontSize: 25,
                               color: signInTextColor,
                               fontWeight: FontWeight.w600)),
-                      SizedBox(height: 40),
+                      SizedBox(height: 30),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
@@ -134,7 +131,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                           decoration: InputDecoration(
                               contentPadding: EdgeInsets.only(left: 20),
                               filled: true,
-                              fillColor: Colors.black,
+                              fillColor: textFieldFillColor,
                               border: UnderlineInputBorder(
                                 borderSide: BorderSide(
                                   color: textFieldBorderColor,
@@ -157,14 +154,14 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                                 ),
                               ),
                               hintStyle: GoogleFonts.openSans(
-                                  fontSize: 13,
+                                  fontSize: 12,
                                   color: hintTextColor,
                                   fontWeight: FontWeight.w600),
                               hintText: 'Enter your email address or phone number'),
                         ),
                       ),
                       SizedBox(
-                        height: 12,
+                        height: 10,
                       ),
                       MaterialButton(
                           onPressed: () {},
@@ -182,7 +179,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                                   color: Colors.white,
                                   fontWeight: FontWeight.w600))),
                       SizedBox(
-                        height: 10,
+                        height: 5,
                       ),
                       RichText(
                         text: TextSpan(
@@ -217,7 +214,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                             ]),
                       ),
                       SizedBox(
-                        height: 50,
+                        height: 30,
                       ),
                       Text('New on our platform?',
                           style: GoogleFonts.openSans(
@@ -235,7 +232,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                             borderRadius: BorderRadius.circular(5),
                             side: BorderSide(width: 2, style: BorderStyle.solid, color: roundedRectangularBorderColor.withOpacity(0.5)),
                           ),
-                          height: 55,
+                          height: 50,
                           color: materialButtonColor,
                           child: Text('Create an Account',
                               style: GoogleFonts.openSans(
@@ -247,7 +244,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                 ),
 
                 SizedBox(
-                  height: 50,
+                  height: 30,
                 ),
                 Divider(
                   color: Color(0xFF232e4b).withOpacity(0.3),
@@ -255,7 +252,7 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                   thickness: 1,
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 40, right: 60.0, left: 60.0),
+                  padding: const EdgeInsets.only(top: 30, right: 60.0, left: 60.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
@@ -277,10 +274,13 @@ class _ScaffoldWidgetState extends State<ScaffoldWidget> {
                     ],
                   ),
                 ),
-                
+
               ],
             ),
           )),
     );
   }
 }
+
+
+
